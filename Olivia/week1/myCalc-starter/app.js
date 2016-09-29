27ad396cb126f361adff06dca298959c43440ba2
Livyfh <opf2000@barnard.edu>
2016-09-29 18:33:59 -0400

    Homework week 1
    completed

---
+++ Olivia/week1/myCalc-starter/app.js
@@ -0,0 +1,27 @@
+console.log('yar! js working.')
+
+var calc= prompt("what type of calculation would you like to make? Multiply, subtract, add, divide?")
+var first= parseInt(prompt("what is your first number?"));
+var second= parseInt(prompt("what is your second number?");
+
+console.log(calc)
+console.log(first)
+console.log(second)
+
+if(calc == "multiply"){
+ alert (first * second)
+}
+
+if(calc == "subtract"){
+   alert (first - second)
+}
+
+if(calc == "add"){
+   alert (first + second)
+ 
+}
+
+if(calc == "divide"){
+   alert (first / second)
+}
+// Your code here!
